f41757088e15bad635b96b701804db5e0e6247a4
Thomas Pendleton <thomas6576@yahoo.com>
2022-06-20 13:55:07 -0700

    finished sidebar context project

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Modal from './Modal'
 import Sidebar from './Sidebar'
 import Home from './Home'
 
-
-
 function App() {
   return (
     <>
-      <h2>modal-sidebar project setup</h2>
+      <Home />
+      <Modal />
+      <Sidebar />
     </>
   )
 }
